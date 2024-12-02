0dbed39a63d4548bb45a9d491131f8cf7c661dfe
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2024-12-01 21:42:34 -0500

    Update AOC_day01_part2.py
    Deleted unnecessary comments

--- AOC 2024 Day 1/AOC_day01_part2.py
+++ AOC 2024 Day 1/AOC_day01_part2.py
@@ -3,8 +3,6 @@
 import pandas as pd
 from collections import Counter
 df = pd.read_csv('AOC 2024 Day 1\AOC_day01_puzzleInput.csv')
-#print("\nBefore sorting:")
-#print(df)
 
 # original csv to list.
 locations = df['locationID'].str.split().tolist()
@@ -18,9 +16,7 @@ convertedRightList = list(map(int, rightList))
 
 # sort smallest - largest each list
 leftSorted = sorted(convertedLeftList)
-#print("\nLeft list sorted: " ,leftSorted)
 rightSorted = sorted(convertedRightList)
-#print("\nRight list sorted: " ,rightSorted)
 
 # need to compare each element of left list individually against right list...
 multipleList = []
@@ -30,14 +26,14 @@ for i in range(len(leftSorted)):
             multipleInstance = rightSorted[j]
             multipleList.append(multipleInstance)
 sortedMultipleList = sorted(multipleList)
-#print(sortedMultipleList)
 
 answer = 0
 answerList = []
+
 # Creates a list of the common elements. 
+
 someList = list(set(leftSorted).intersection(rightSorted))
 sortedSomeList = sorted(someList)
-#print("\nSorted Some List:" ,sortedSomeList)
 for i in range(len(sortedSomeList)):
     for j in range(len(sortedMultipleList)):
         if (sortedSomeList[i] == sortedMultipleList[j]):
