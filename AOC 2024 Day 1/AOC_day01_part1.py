e07d43c3dc6ee9a214fc56ff4a740405dd2f1088
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2024-12-01 21:29:46 -0500

    AOC day1
    2-Gold stars for day 1

---
+++ AOC 2024 Day 1/AOC_day01_part1.py
@@ -0,0 +1,38 @@
+# AOC 2024 Day 1: Historian Hysteria
+# Part 1:
+import pandas as pd
+import csv
+import operator
+df = pd.read_csv('AOC 2024 Day 1\AOC_day01_puzzleInput.csv')
+#print("\nBefore sorting:")
+#print(df)
+
+# original csv to list.
+locations = df['locationID'].str.split().tolist()
+
+# code to split into 2 separate lists
+leftList, rightList = map(list, zip(*locations))
+
+# Convert the string lists to int!
+convertedLeftList = list(map(int, leftList))
+convertedRightList = list(map(int, rightList))
+
+# sort smallest - largest each list
+leftSorted = sorted(convertedLeftList)
+rightSorted = sorted(convertedRightList)
+
+# find the difference between the sorted lists
+    #for each element:
+        #Subtract smaller from larger,
+        #Then add all differences...That's the answer. 
+answerList = []
+for i in range(len(leftSorted)):
+    if (leftSorted[i] > rightSorted[i]):
+        distance = leftSorted[i] - rightSorted[i]
+        answerList.append(distance)
+    elif (leftSorted[i] < rightSorted[i]):
+        distance = rightSorted[i] - leftSorted[i]
+        answerList.append(distance)
+
+print("\nThe total distance: ", sum(answerList))
+# Correct Answer: 2904518
---
+++ AOC 2024 Day 1/AOC_day01_part2.py
@@ -0,0 +1,48 @@
+# AOC 2024 Day 1: Historian Hysteria
+# Part 1:
+import pandas as pd
+from collections import Counter
+df = pd.read_csv('AOC 2024 Day 1\AOC_day01_puzzleInput.csv')
+#print("\nBefore sorting:")
+#print(df)
+
+# original csv to list.
+locations = df['locationID'].str.split().tolist()
+
+# code to split into 2 separate lists
+leftList, rightList = map(list, zip(*locations))
+
+# Convert the string lists to int!
+convertedLeftList = list(map(int, leftList))
+convertedRightList = list(map(int, rightList))
+
+# sort smallest - largest each list
+leftSorted = sorted(convertedLeftList)
+#print("\nLeft list sorted: " ,leftSorted)
+rightSorted = sorted(convertedRightList)
+#print("\nRight list sorted: " ,rightSorted)
+
+# need to compare each element of left list individually against right list...
+multipleList = []
+for i in range(len(leftSorted)):
+    for j in range(len(rightSorted)):
+        if(leftSorted[i] == rightSorted[j]):
+            multipleInstance = rightSorted[j]
+            multipleList.append(multipleInstance)
+sortedMultipleList = sorted(multipleList)
+#print(sortedMultipleList)
+
+answer = 0
+answerList = []
+# Creates a list of the common elements. 
+someList = list(set(leftSorted).intersection(rightSorted))
+sortedSomeList = sorted(someList)
+#print("\nSorted Some List:" ,sortedSomeList)
+for i in range(len(sortedSomeList)):
+    for j in range(len(sortedMultipleList)):
+        if (sortedSomeList[i] == sortedMultipleList[j]):
+            countedList = sortedMultipleList.count(sortedSomeList[i])
+            answer = countedList * sortedSomeList[i]
+    answerList.append(answer)
+print("\nSimilarity score: ",sum(answerList))
+# Correct Answer: 18650129
